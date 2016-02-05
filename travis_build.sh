9e9860b516b9e6e65b9edc9e3be17e848830138f
Felix Hupfeld <felix@quobyte.com>
2016-02-05 20:11:10 +0100

    Remove Mesos version specific builds

--- travis_build.sh
+++ travis_build.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 set -ev
-export MESOS_VERSION=0.24.1
-./build.sh
-./push_docker.sh
-
 export MESOS_VERSION=0.25.0
-./build.sh
-./push_docker.sh
-
-export MESOS_VERSION=0.26.0
-./build.sh
-./push_docker.sh
-
-export MESOS_VERSION=0.27.0
 ./build.sh
 ./push_docker.sh
