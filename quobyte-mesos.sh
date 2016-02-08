b238a37631cd04f03181fdb1e4d993c5e3c4297e
Silvan Kaiser <silvan@quobyte.com>
2016-02-08 16:00:50 +0100

    Adds framework_image parameter to start script.

--- quobyte-mesos.sh
+++ quobyte-mesos.sh
@@ -4,7 +4,8 @@ echo "Mesos master: $MESOS_MASTER"
 echo "HTTP port: $PORT"
 echo "Docker image: $DOCKER_IMAGE"
 echo "mesos-dns domain: $MESOS_DNS_DOMAIN"
+echo "framework image: $FRAMEWORK_IMAGE"
 
 export LD_LIBRARY_PATH=/opt:$LD_LIBRARY_PATH 
 cd /opt
-/opt/quobyte-mesos --zk=$ZOOKEEPER --master=$MESOS_MASTER --port=$PORT --docker_image=$DOCKER_IMAGE --mesos_dns_domain=$MESOS_DNS_DOMAIN
+/opt/quobyte-mesos --zk=$ZOOKEEPER --master=$MESOS_MASTER --port=$PORT --docker_image=$DOCKER_IMAGE --mesos_dns_domain=$MESOS_DNS_DOMAIN --framework_image=$FRAMEWORK_IMAGE
